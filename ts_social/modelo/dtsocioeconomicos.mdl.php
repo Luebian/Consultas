4a78f20c235320a209a316116ddf65706e6c4557
Chad <payments@payapi.us>
2017-10-03 13:34:47 -0400

    fix sqli
    fix sqli

--- ts_social/modelo/dtsocioeconomicos.mdl.php
+++ ts_social/modelo/dtsocioeconomicos.mdl.php
@@ -3,25 +3,25 @@
 class deconomicos {
         var $error = 0;
         function registro(){
-                      $sql="INSERT INTO ts_infoecono VALUES('{$_POST["conce"]}',
-                                                         '{$_POST["mensual"]}',
-                                                         '{$_POST["bnosdesp"]}',
-                                                         '{$_POST["prima"]}',
-                                                         '{$_POST["vacas"]}',
-                                                         '{$_POST["conce2"]}',
-                                                         '{$_POST["rthipote"]}',
-                                                         '{$_POST["crede"]}',
-                                                         '{$_POST["servi"]}',
-                                                         '{$_POST["mdl"]}',
-                                                         '{$_POST["ano"]}',
-                                                         '{$_POST["valor"]}',
-                                                         '{$_POST["pro"]}',
-                                                         '{$_POST["emp"]}',
-                                                         '{$_POST["ubi"]}',
-                                                         '{$_POST["mconstruc"]}',
-                                                         '{$_POST["mterreno"]}',
-                                                         '{$_POST["constr"]}',        
-                                                        '{$_POST["cost"]}')";
+                      $sql="INSERT INTO ts_infoecono VALUES('{mysql_real_escape_string($_POST["conce"])}',
+                                                         '{mysql_real_escape_string($_POST["mensual"])}',
+                                                         '{mysql_real_escape_string($_POST["bnosdesp"])}',
+                                                         '{mysql_real_escape_string($_POST["prima"])}',
+                                                         '{mysql_real_escape_string($_POST["vacas"])}',
+                                                         '{mysql_real_escape_string($_POST["conce2"])}',
+                                                         '{mysql_real_escape_string($_POST["rthipote"])}',
+                                                         '{mysql_real_escape_string($_POST["crede"])}',
+                                                         '{mysql_real_escape_string($_POST["servi"])}',
+                                                         '{mysql_real_escape_string($_POST["mdl"])}',
+                                                         '{mysql_real_escape_string($_POST["ano"])}',
+                                                         '{mysql_real_escape_string($_POST["valor"])}',
+                                                         '{mysql_real_escape_string($_POST["pro"])}',
+                                                         '{mysql_real_escape_string($_POST["emp"])}',
+                                                         '{mysql_real_escape_string($_POST["ubi"])}',
+                                                         '{mysql_real_escape_string($_POST["mconstruc"])}',
+                                                         '{mysql_real_escape_string($_POST["mterreno"])}',
+                                                         '{mysql_real_escape_string($_POST["constr"])}',        
+                                                        '{mysql_real_escape_string($_POST["cost"])}')";
                                                         
                  //echo $sql;
                     //if ($error == 0) {
